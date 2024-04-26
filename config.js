593fd04c43fac2487c66e192a6e5aeb78cc428c7
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-04-26 01:15:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  ['212641207087', 'WHITE', true], 
+  ['212641207087', 'Abraham Dwamena', true],
+  ['212641207087', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['212641207087', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['212641207087', 'Abraham Dwamena2', true],
+  ['212641207087', 'Abraham Dwamena3', true],
   
 ] //Number of owners
 
@@ -55,10 +55,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'MEE5'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'MEE5' 
+global.author = '@MEE5' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
